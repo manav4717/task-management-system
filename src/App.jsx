72550e6dd3c93a44105da6a960d092d3bb62b5b0
Manav Goswami <manavbharthigoswami29@gmail.com>
2026-02-02 13:38:06 +0530

    [MG][file]

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
- import Header from './components/Header'
-import './App.css'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Header from "./components/Header";
+import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
+const DefaultRoute = () => {
+  const authData = JSON.parse(localStorage.getItem("authData"));
+  if (authData) {
+    return <Navigate to="/login" replace />;
+  }
+  return <Navigate to="/register" replace />;
+};
 function App() {
   const route = createBrowserRouter([
     {
+      path: "/",
+      element: <DefaultRoute />,
+    },
+    {
       path: "/login",
-      element: <Login />
+      element: <Login />,
     },
     {
       path: "/register",
-      element: <Register />
-    }
-  ])
+      element: <Register />,
+    },
+  ]);
 
-  return<RouterProvider router={route}/>
+  return <RouterProvider router={route} />;
 }
 
-export default App
+export default App;
