ac372ee24c24182db7c2db4244b5f47905019524
Manav Goswami <manavbharthigoswami29@gmail.com>
2026-02-05 08:12:22 +0530

    [MG][page]

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <>
+      <div className="task-grid">
+        {/*Task Card 1*/}
+
+        <div className="task-card" style={{ position:'relative'}}>
+            <h3>Complete React Assignment</h3>
+            <p>Finish task manager UI and styling</p>
+
+            <div className="task-meta">
+                <span>Due : 2026-02-10</span>
+                <span className="priority-badge priority-high">High</span>
+            </div>
+
+            <div className="task-actions">
+                <button 
+                    className="btn-icon"
+                    style={{background:'#00d2ff'}}
+                    title="Edit Task"
+                    >
+                        🖌️
+                    </button>
+
+                    <button
+                       className="btn-icon"
+                       style={{background : '#00b894'}}
+                       title="Mark Complete"
+                       >
+                        👌
+                       </button>
+
+                       <button 
+                           className="btn-icon"
+                           style={{background : '#ff416c'}}
+                           title="Delete Task"
+                           >
+                            🗑️
+                           </button>
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TaskList
--- src/pages/Daskbord.jsx
+++ src/pages/Daskbord.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+// ...existing code...
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import { useNavigate } from 'react-router-dom'
+import TaskList from '../components/TaskList'
 
-const Daskbord = () => {
-return (
-<div>
-<h1>hello React…!</h1>
-</div>
-)
+const Dashboard = () => {
+
+  const navigate = useNavigate()
+  const [tasks, setTasks] = useState([])
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
+  const fetchData = async() =>{
+    try {
+      const response = await fetch("http://localhost:3000/tasks");
+      const data = await response.json();
+      setTasks(data);
+    }catch(error) {
+      console.log(error)
+    }
+  };
+
+  const handleLogout = () => {
+    console.log('click from dashboard')
+    localStorage.removeItem('loginData')
+    localStorage.removeItem('authData')
+    navigate('/login')
+  }
+
+  return (
+    <div>
+      <Navbar title="Task Management" onLogout={handleLogout}/>
+      <h1> Hello </h1>
+      <TaskList tasks={tasks} />
+    </div>
+  )
 }
 
-export default Daskbord
-
-
+export default Dashboard
